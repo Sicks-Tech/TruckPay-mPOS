a91149113a9523c4759bb13381977f8ca6f7a5b1
jesse <nitchsx@gmail.com>
2026-01-16 01:18:34 -0300

    Add payment process and confirmation activities
    - Create `PaymentProcessActivity` and its layout `activity_payment_process.xml` to display transaction status.
    - Create `ConfirmationActivity` and its layout `activity_confirmation.xml` featuring a success message and logo.
    - Set `ConfirmationActivity` as the main launcher activity in `AndroidManifest.xml`.
    - Update `AndroidManifest.xml` to register the new activities and adjust existing activity exports.
    - Implement toolbar setup and window insets handling in `PaymentProcessActivity`.
    - Remove experimental IDE render settings and update deployment target configuration.

---
+++ app/src/main/java/com/jesse/sickstech/features/paymentProcess/PaymentProcessActivity.kt
@@ -0,0 +1,36 @@
+package com.jesse.sickstech.features.paymentProcess
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.navigation.findNavController
+import com.jesse.sickstech.R
+import com.jesse.sickstech.core.util.setupToolbar
+import com.jesse.sickstech.databinding.ActivityPaymentProcessBinding
+
+class PaymentProcessActivity : AppCompatActivity() {
+    val binding by lazy {
+        ActivityPaymentProcessBinding.inflate(layoutInflater)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(binding.root)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        with(binding){
+            includeToolbar.setupToolbar(
+                title = "Pagamento",
+                showKeyboard = false,
+                onBack = {root.findNavController().popBackStack()}
+            )
+        }
+    }
+}
